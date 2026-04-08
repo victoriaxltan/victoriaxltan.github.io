dd3cddea0c955e295f288cbda81b47dc44d00b02
victoriaxltan <57047989+victoriaxltan@users.noreply.github.com>
2026-04-08 12:51:06 +0000

    Deploying to gh-pages from @ victoriaxltan/victoriaxltan.github.io@48cf2938dfe65b792d5689ae7168dec4de2411c3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "victoriaxltan/victoriatan.github.io",
+    "data-repo": "victoriaxltan/victoriaxltan.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
